23c8cce7e28ff16235e329c015607427cdb98a24
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2023-07-09 17:00:19 +0300

    - cleaned package definition

--- Package.swift
+++ Package.swift
@@ -4,17 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "RetroSwift",
-    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "RetroSwift",
             targets: ["RetroSwift"]),
     ],
-    dependencies: [],
     targets: [
         .target(
             name: "RetroSwift",
-            dependencies: [],
             path: "RetroSwift"),
         .testTarget(
             name: "RetroSwiftTests",
