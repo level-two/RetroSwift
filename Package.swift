5d5993ee2051d9288e3bcade11a59f858db38a54
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2023-07-27 15:40:16 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "RetroSwift",
+    platforms: [
+        .macOS("10.15"),
+        .iOS("13.0"),
+        .tvOS("13.0"),
+        .watchOS("6.0")
+    ],
     products: [
         .library(
             name: "RetroSwift",
